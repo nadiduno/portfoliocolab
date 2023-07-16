dd647f29b8c66e60da961c1a26f3bd238a06815d
Nadi Duno <nadiduno@gmail.com>
2023-07-16 14:09:25 -0300

    Atualizando JS

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
-console.log("cargando")
-async function UserData () {
-    var UserId = document.getElementById("UserId").value
-    var urlApi = `https://api.github.com/users/${UserId}`
+async function userData() {
+    var userId = document.getElementById("userId").value
+    var urlApi = `https://api.github.com/users/${userId}`
 
-    await fetch(urlApi) 
-
+    await fetch(urlApi)
         .then((response) => {
             return response.json();
-            
         })
+
         .then((data) => {
-            console.log(data)
+            // console.log(data)
 
-            
-            // let imgPokemonFront = data["sprites"]["front_default"]
-            
-
-            document.getElementById("Username").innerHTML = data["name"]
-            document.getElementById("AvatarUser").setAttribute ('src', data["avatar_url"])
-          
-
+            document.getElementById("userAvatar").setAttribute('src', data["avatar_url"])
+            document.getElementById("userName").innerHTML = data["name"]
+            document.getElementById("userBio").innerHTML = data["bio"]
+            document.getElementById("publicRepos").innerHTML = data["public_repos"]
+            document.getElementById("followers").innerHTML = data["followers"]
+            document.getElementById("following").innerHTML = data["following"]
+            document.getElementById("htmlUrl").setAttribute('href', data["html_url"])
+            document.getElementById("menssage").innerHTML = "conta criada desde"
+            document.getElementById("createdAt").innerHTML = data["created_at"]
         })
         .catch((error) => {
             console.error("Erro API Github")
